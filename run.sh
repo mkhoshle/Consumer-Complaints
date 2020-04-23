f633eaa17fc3365d691f711bf0c1f68a360f4b6b
Mahzad Khoshlessan <mahzad.khoshlessan@asu.edu>
2020-04-22 23:38:40 -0500

    Added test files

--- run.sh
+++ run.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-<<<<<<< HEAD
 #
 echo "First we check the code to see if it pass the tests" 
 python src/test_complaint_analysis.py -v
-=======
-
-#echo "First we check the code to see if it pass the tests" 
-#python src/test_complaint_analysis.py -v
->>>>>>> 7208cdf2ceaf0b52f19b268eb70992502b1d1337
 
 echo "Running the main code to produce the result!"
 python3.8 src/run.py --ipath 'input/complaints.csv' --opath 'output/report.csv'
