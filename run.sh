fb4233551296a7279c99976e1036d63a14bc818b
Mahzad Khoshlessan <mkhoshle@asu.edu>
2020-04-22 23:26:55 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-#
-echo "Installing the required packages"
-#wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
-#bash Miniconda3-latest-MacOSX-x86_64.sh
-pip install numpy
-pip install pandas
-
 
 echo "First we check the code to see if it pass the tests" 
 python src/test_complaint_analysis.py -v
