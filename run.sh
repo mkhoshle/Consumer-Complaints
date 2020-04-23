5b97f7b2311946a33dfe9a07beb300d9af6adcab
Mahzad Khoshlessan <mkhoshle@asu.edu>
2020-04-22 23:40:15 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-echo "First we check the code to see if it pass the tests" 
-python src/test_complaint_analysis.py -v
+#echo "First we check the code to see if it pass the tests" 
+#python src/test_complaint_analysis.py -v
 
 echo "Running the main code to produce the result!"
 python3.8 src/run.py --ipath 'input/complaints.csv' --opath 'output/report.csv'
